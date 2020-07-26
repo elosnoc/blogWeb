8cb7b113bd32614ca0b420f0c788719448b4a61c
elosnoc <stinkyzwork@gmail.com>
2020-07-26 11:56:31 +0700

    Create Blog-form

---
+++ myblog/routes/blog.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('blogs/index');
+});
+
+router.get('/add', function(req, res, next) {
+    res.render('blogs/blogsAdd');
+  });
+
+module.exports = router;
